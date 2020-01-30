f4efd1b7277f0e60fd16f699f8fe526584a89a2e
Roberto Aguero <rjaguero00@gmail.com>
2020-01-29 19:18:51 -0600

    CONCAT

--- Lesson 1-7/Lesson 5.sql
+++ Lesson 1-7/Lesson 5.sql
@@ -37,12 +37,38 @@ QUIZ POSITION, STRPOS, & SUBSTR
 
 -- QUESTION 1
 
-SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first_name,
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) AS first_name,
 RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name
 FROM accounts;
 
 -- QUESTION 2
 
-SELECT LEFT(name, STRPOS(name, ' ') - 1) AS first_name,
+SELECT LEFT(name, STRPOS(name, ' ') -1) AS first_name,
 RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) last_name
 FROM sales_reps;
+
+-- QUIZ 3 CONCAT
+
+-- QUESTION 1
+WITH t1 AS (
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) first_name,
+RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com')
+FROM t1;
+
+-- QUESTION 2
+WITH t1 AS (
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) first_name,
+RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', REPLACE(name, ' ', ''), '.com')
+FROM t1;
+
+-- QUESTION 3
+
+WITH t1 AS (
+ SELECT LEFT(primary_poc,     STRPOS(primary_poc, ' ') -1 ) first_name,  RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+ FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com'), LEFT(LOWER(first_name), 1) || RIGHT(LOWER(first_name), 1) || LEFT(LOWER(last_name), 1) || RIGHT(LOWER(last_name), 1) || LENGTH(first_name) || LENGTH(last_name) || REPLACE(UPPER(name), ' ', '')
+FROM t1;
