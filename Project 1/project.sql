c166b769d9c527dd8cd992ba20daf3f2a2045d90
Roberto Aguero <rjaguero00@gmail.com>
2020-02-05 14:06:10 -0600

    project Q1

--- Project 1/project.sql
+++ Project 1/project.sql
@@ -8,7 +8,23 @@ FROM (SELECT f.title, c.name
     ORDER BY 2) t1
 GROUP BY 1;
 
--- 1. 
+-- 1. What are the Top 5 rented categories?
+
+SELECT f.title, c.name, COUNT(r.rental_id)
+FROM film f, category c, film_category fc, rental r, inventory i
+WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND c.name IN ('Action', 'Animation', 'Children', 'Classics', 'Comedy', 'Documentary', 'Drama', 'Foreign', 'Games', 'Horror', 'New', 'Sci-Fi', 'Sports', 'Travel', 'Family', 'Music')
+GROUP BY 1, 2
+ORDER BY 2, 3 DESC;
+
+-- 2. What are the total sales for these top 5 categories or what categories bring in the most sales?
+
+
+
+
+
+
+
+
 
 SELECT f.title, c.name, COUNT(r.rental_id)
 FROM film f, category c, film_category fc, rental r, inventory i
@@ -16,4 +32,61 @@ WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
+
+
+SELECT name, COUNT(name) cat_total
+FROM (SELECT f.title, c.name
+    FROM film f, category c, film_category fc
+    WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND c.name IN ('Action', 'Animation', 'Children', 'Classics', 'Comedy', 'Documentary', 'Drama', 'Foreign', 'Games', 'Horror', 'New', 'Sci-Fi', 'Sports', 'Travel', 'Family', 'Music')
+    GROUP BY 1, 2
+    ORDER BY 2) t1
+GROUP BY 1;
+
+
+SELECT name, COUNT(name) cat_total
+FROM (SELECT f.title, c.name, COUNT(r.rental_id)
+    FROM film f, category c, film_category fc, rental r, inventory i
+    WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND c.name IN ('Action', 'Animation', 'Children', 'Classics', 'Comedy', 'Documentary', 'Drama', 'Foreign', 'Games', 'Horror', 'New', 'Sci-Fi', 'Sports', 'Travel', 'Family', 'Music')
+    GROUP BY 1, 2
+    ORDER BY 2, 3 DESC) t1
+GROUP BY 1
+ORDER BY 2 DESC;
+
+
+
+
 -- 2. 
+SELECT name, COUNT(name) cat_total
+FROM (SELECT f.title, c.name
+    FROM film f, category c, film_category fc
+    WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+    GROUP BY 1, 2
+    ORDER BY 2) t1
+GROUP BY 1;
+
+
+SELECT f.title, c.name, COUNT(r.rental_id)
+FROM film f, category c, film_category fc, rental r, inventory i
+WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+SELECT f.title, c.name, COUNT(r.rental_id)
+FROM film f, category c, film_category fc, rental r, inventory i
+WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND c.name IN ('Action', 'Animation', 'Children', 'Classics', 'Comedy', 'Documentary', 'Drama', 'Foreign', 'Games', 'Horror', 'New', 'Sci-Fi', 'Sports', 'Travel', 'Family', 'Music')
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+SELECT f.title, c.name, COUNT(r.rental_id)
+FROM film f, category c, film_category fc, rental r, inventory i
+WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+SELECT name, COUNT(name) cat_total
+FROM (SELECT f.title, c.name
+    FROM film f, category c, film_category fc
+    WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+    GROUP BY 1, 2
+    ORDER BY 2) t1
+GROUP BY 1;
