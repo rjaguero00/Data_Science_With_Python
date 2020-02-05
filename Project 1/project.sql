49777d5c8c076992bb7462f8094d77d21333c165
Roberto Aguero <rjaguero00@gmail.com>
2020-02-05 15:14:40 -0600

    Q2

--- Project 1/project.sql
+++ Project 1/project.sql
@@ -10,14 +10,47 @@ GROUP BY 1;
 
 -- 1. What are the Top 5 rented categories?
 
-SELECT f.title, c.name, COUNT(r.rental_id)
+SELECT f.title, c.name, COUNT(r.rental_id) retntal_count
 FROM film f, category c, film_category fc, rental r, inventory i
 WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND c.name IN ('Action', 'Animation', 'Children', 'Classics', 'Comedy', 'Documentary', 'Drama', 'Foreign', 'Games', 'Horror', 'New', 'Sci-Fi', 'Sports', 'Travel', 'Family', 'Music')
 GROUP BY 1, 2
 ORDER BY 2, 3 DESC;
 
 -- 2. What are the total sales for these top 5 categories or what categories bring in the most sales?
+-- Total sales from all categories
 
+SELECT c.name, SUM(p.amount) total_rev
+FROM category c, film_category fc, film f, inventory i, rental r, payment p
+WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND p.rental_id = r.rental_id
+GROUP BY 1
+ORDER BY 2 DESC;
+
+
+WITH
+    t1
+    AS
+    (
+        SELECT c.name categories, COUNT(r.rental_id) retntal_count
+        FROM film f, category c, film_category fc, rental r, inventory i
+        WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id
+        GROUP BY 1
+        ORDER BY 2 DESC
+    ),
+
+    t2
+    AS
+    (
+        SELECT c.name categories, SUM(p.amount) total_rev
+        FROM category c, film_category fc, film f, inventory i, rental r, payment p
+        WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND r.inventory_id = i.inventory_id AND f.film_id = i.film_id AND p.rental_id = r.rental_id
+        GROUP BY 1
+        ORDER BY 2 DESC
+    )
+
+SELECT t1.categories, t1.retntal_count, t2.total_rev
+FROM t1, t2
+WHERE t1.categories = t2.categories
+ORDER BY 3 DESC;
 
 
 
