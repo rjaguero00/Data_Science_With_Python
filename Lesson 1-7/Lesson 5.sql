66ce3377791c64594a1ec03436e08c516fd800f2
Roberto Aguero <rjaguero00@gmail.com>
2020-02-05 18:36:36 -0600

    fixes

--- Lesson 1-7/Lesson 5.sql
+++ Lesson 1-7/Lesson 5.sql
@@ -18,10 +18,10 @@ ORDER BY 2 DESC;
 
 SELECT SUM(num) nums, SUM(letter) letters
 FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
-                       THEN 1 ELSE 0 END AS num, 
-         CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
+                       THEN 1 ELSE 0 END AS num,
+        CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
                        THEN 0 ELSE 1 END AS letter
-      FROM accounts) t1;
+    FROM accounts) t1;
 
 
 -- QUESTION 4
@@ -29,7 +29,7 @@ FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6
 SELECT SUM(vowel) vowels, SUM(nope) no_vowel
 FROM (
     SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 1 ELSE 0 END AS vowel,
-    CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 0 ELSE 1 END AS nope
+        CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 0 ELSE 1 END AS nope
     FROM accounts
 ) t1;
 
@@ -38,38 +38,50 @@ QUIZ POSITION, STRPOS, & SUBSTR
 -- QUESTION 1
 
 SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) AS first_name,
-RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name
+    RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name
 FROM accounts;
 
 -- QUESTION 2
 
 SELECT LEFT(name, STRPOS(name, ' ') -1) AS first_name,
-RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) last_name
+    RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) last_name
 FROM sales_reps;
 
 -- QUIZ 3 CONCAT
 
 -- QUESTION 1
-WITH t1 AS (
-SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) first_name,
-RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
-FROM accounts)
+WITH
+    t1
+    AS
+    (
+        SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) first_name,
+            RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+        FROM accounts
+    )
 SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com')
 FROM t1;
 
 -- QUESTION 2
-WITH t1 AS (
-SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) first_name,
-RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
-FROM accounts)
+WITH
+    t1
+    AS
+    (
+        SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) first_name,
+            RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+        FROM accounts
+    )
 SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', REPLACE(name, ' ', ''), '.com')
 FROM t1;
 
 -- QUESTION 3
 
-WITH t1 AS (
- SELECT LEFT(primary_poc,     STRPOS(primary_poc, ' ') -1 ) first_name,  RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
- FROM accounts)
+WITH
+    t1
+    AS
+    (
+        SELECT LEFT(primary_poc,     STRPOS(primary_poc, ' ') -1 ) first_name, RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+        FROM accounts
+    )
 SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com'), LEFT(LOWER(first_name), 1) || RIGHT(LOWER(first_name), 1) || LEFT(LOWER(last_name), 1) || RIGHT(LOWER(last_name), 1) || LENGTH(first_name) || LENGTH(last_name) || REPLACE(UPPER(name), ' ', '')
 FROM t1;
 
@@ -80,7 +92,8 @@ FROM t1;
 
 SELECT *
 FROM sf_crime_data
-LIMIT 10;
+LIMIT
+10;
 
 -- QUESTION 2
 
@@ -89,7 +102,8 @@ FROM sf_crime_data;
 
 --QUESTION 3
 
-SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2))::DATE new_date
+SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2))
+::DATE new_date
 FROM sf_crime_data;
 
 -- QUIZ COALESCE
@@ -98,32 +112,32 @@ FROM sf_crime_data;
 
 SELECT *
 FROM accounts a
-LEFT JOIN orders o
-ON a.id = o.account_id
-WHERE o.total IS NULL; 
+    LEFT JOIN orders o
+    ON a.id = o.account_id
+WHERE o.total IS NULL;
 
 -- QUESTION 2
 
 SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, o.*
 FROM accounts a
-LEFT JOIN orders o
-ON a.id = o.account_id
+    LEFT JOIN orders o
+    ON a.id = o.account_id
 WHERE o.total IS NULL;
 
 -- QUESTION 3
 
 SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, o.occurred_at, o.standard_qty, o.gloss_qty, o.poster_qty, o.total, o.standard_amt_usd, o.gloss_amt_usd, o.poster_amt_usd, o.total_amt_usd
 FROM accounts a
-LEFT JOIN orders o
-ON a.id = o.account_id
+    LEFT JOIN orders o
+    ON a.id = o.account_id
 WHERE o.total IS NULL;
 
 -- QUESTION 4
 
 SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, o.occurred_at, COALESCE(o.standard_qty, 0) standard_qty, COALESCE(o.gloss_qty,0) gloss_qty, COALESCE(o.poster_qty,0) poster_qty, COALESCE(o.total,0) total, COALESCE(o.standard_amt_usd,0) standard_amt_usd, COALESCE(o.gloss_amt_usd,0) gloss_amt_usd, COALESCE(o.poster_amt_usd,0) poster_amt_usd, COALESCE(o.total_amt_usd,0) total_amt_usd
 FROM accounts a
-LEFT JOIN orders o
-ON a.id = o.account_id
+    LEFT JOIN orders o
+    ON a.id = o.account_id
 WHERE o.total IS NULL;
 
 -- QUESTION 5
--- Lesson 1-7/subquery.sql
+++ Lesson 1-7/subquery.sql
@@ -1,34 +1,44 @@
 -- QUESTION 1
 
-WITH t1 AS (
-   SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
-FROM sales_reps s, accounts a, orders o, region r
-WHERE s.region_id = r.id AND s.id = a.sales_rep_id AND a.id = o.account_id
-GROUP BY 1,2
-ORDER BY 3 DESC
-),
+WITH
+    t1
+    AS
+    (
+        SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+        FROM sales_reps s, accounts a, orders o, region r
+        WHERE s.region_id = r.id AND s.id = a.sales_rep_id AND a.id = o.account_id
+        GROUP BY 1,2
+        ORDER BY 3 DESC
+    ),
 
-WITH t2 AS (
-    SELECT region_name, MAX(total_amt) total_amt
-    FROM t1
-    GROUP BY 1
-)
+    t2
+    AS
+    (
+        SELECT region_name, MAX(total_amt) total_amt
+        FROM t1
+        GROUP BY 1
+    )
 
 SELECT t1.rep_name, t1.region_name, t1.total_amt
 FROM t1
-JOIN t2
-ON t1.region_name = t2.region_name AND t1.total_amt = t2.total_amt;
+    JOIN t2
+    ON t1.region_name = t2.region_name AND t1.total_amt = t2.total_amt;
 
 -- QUESTION 2
 
-WITH t1 AS (
-    SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
-    FROM sales_reps s, accounts a, orders o, region r
-    WHERE s.region_id = r.id AND s.id = a.sales_rep_id AND a.id = o.account_id
-    GROUP BY r.name
-),
+WITH
+    t1
+    AS
+    (
+        SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
+        FROM sales_reps s, accounts a, orders o, region r
+        WHERE s.region_id = r.id AND s.id = a.sales_rep_id AND a.id = o.account_id
+        GROUP BY r.name
+    ),
 
-    t2 AS (
+    t2
+    AS
+    (
         SELECT MAX(total_amt)
         FROM t1
     )
@@ -37,25 +47,33 @@ SELECT r.name, COUNT(o.total) total_orders
 FROM sales_reps s, accounts a, orders o, region r
 WHERE s.region_id = r.id AND s.id = a.sales_rep_id AND a.id = o.account_id
 GROUP BY r.name
-HAVING SUM(o.total_amt_usd) = (SELECT * FROM t2);
+HAVING SUM(o.total_amt_usd) = (SELECT *
+FROM t2);
 
 -- QUESTION 3
 
-WITH t1 AS (
-    SELECT a.name account_name, SUM(o.standard_qty) total_std, SUM(o.total) total
-FROM accounts a, orders o
-WHERE a.id = o.account_id
-GROUP BY 1
-ORDER BY 2 DESC
+WITH
+    t1
+    AS
+    (
+        SELECT a.name account_name, SUM(o.standard_qty) total_std, SUM(o.total) total
+        FROM accounts a, orders o
+        WHERE a.id = o.account_id
+        GROUP BY 1
+        ORDER BY 2 DESC
 LIMIT 1
 ),
 
-t2 AS (
+t2
+    
+     AS
+(
     SELECT a.name
 FROM accounts a, orders o
 WHERE a.id = o.account_id
-GROUP BY 1 
-HAVING SUM(o.total) > (SELECT total FROM t1)
+GROUP BY 1
+HAVING SUM(o.total) > (SELECT total
+FROM t1)
 )
 
 SELECT COUNT (*)
@@ -65,47 +83,67 @@ FROM t2;
 -- QUESTION 4
 
 
-WITH t1 AS (
-    SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
-FROM accounts a, orders o
-WHERE a.id = o.account_id
-GROUP BY 1, 2
-ORDER BY 3 DESC
+WITH
+    t1
+    AS
+    (
+        SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+        FROM accounts a, orders o
+        WHERE a.id = o.account_id
+        GROUP BY 1, 2
+        ORDER BY 3 DESC
 LIMIT 1
 )
 
-SELECT a.name, w.channel, COUNT (*)
+SELECT a
+    .name, w.channel, COUNT
+(*)
 FROM accounts a, web_events w
-WHERE a.id = w.account_id AND a.id = (SELECT id FROM t1)
+WHERE a.id = w.account_id AND a.id =
+(SELECT id
+FROM t1)
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
 -- QUESTION 5
 
-WITH t1 AS (
-  SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
-FROM accounts a, orders o
-WHERE a.id = o.account_id
-GROUP BY 1, 2
-ORDER BY 3 DESC
+WITH
+    t1
+    AS
+    (
+        SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+        FROM accounts a, orders o
+        WHERE a.id = o.account_id
+        GROUP BY 1, 2
+        ORDER BY 3 DESC
 LIMIT 10  
 )
 
-SELECT AVG (tot_spent)
+SELECT AVG
+    
+    
+    
+     (tot_spent)
 FROM t1;
 
 -- QUESTION 6
 
-WITH t1 AS (
-    SELECT AVG(o.total_amt_usd) avg_all
-FROM orders o, accounts a
-WHERE a.id = o.account_id
-),
-t2 AS (
-    SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
-    FROM orders o
-    GROUP BY 1
-    HAVING AVG(o.total_amt_usd) > (SELECT * FROM t1)
-)
+WITH
+    t1
+    AS
+    (
+        SELECT AVG(o.total_amt_usd) avg_all
+        FROM orders o, accounts a
+        WHERE a.id = o.account_id
+    ),
+    t2
+    AS
+    (
+        SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+        FROM orders o
+        GROUP BY 1
+        HAVING AVG(o.total_amt_usd) > (SELECT *
+        FROM t1)
+    )
 SELECT AVG(avg_amt)
 FROM t2;
